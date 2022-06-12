5042d2757b87d9746e1cee2b027e68e2d020b27c
Hudsn <hudsnonline@gmail.com>
2022-06-12 15:47:11 -0400

    test change

--- background.js
+++ background.js
@@ -1,34 +1,33 @@
 chrome.contextMenus.create({
-    title:"URL Defang",
-    contexts:["all"],
-    id:"URLDefang"
+  title: "URL Defang",
+  contexts: ["all"],
+  id: "URLDefang",
 });
 
 chrome.contextMenus.create({
-    title:"Copy Defanged",
-    contexts:["all"],
-    parentId:"URLDefang",
-    id:"copyDefang"
+  title: "Copy Defanged",
+  contexts: ["all"],
+  parentId: "URLDefang",
+  id: "copyDefang",
 });
 
 chrome.contextMenus.create({
-    title:"Copy Refanged",
-    contexts:["all"],
-    parentId:"URLDefang",
-    id:"copyRefang"
-})
+  title: "Copy Refanged",
+  contexts: ["all"],
+  parentId: "URLDefang",
+  id: "copyRefang",
+});
 
-chrome.contextMenus.onClicked.addListener(e => {
-    if (e.menuItemId == 'copyDefang'){
-        sendRightClickEvent('copyDefang')
-    }
-    else if (e.menuItemId == 'copyRefang'){
-        sendRightClickEvent('copyRefang')
-    }
-})
+chrome.contextMenus.onClicked.addListener((e) => {
+  if (e.menuItemId == "copyDefang") {
+    sendRightClickEvent("copyDefang");
+  } else if (e.menuItemId == "copyRefang") {
+    sendRightClickEvent("copyRefang");
+  }
+});
 
-function sendRightClickEvent(clickType){
-    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id, {defangEvent: clickType})
-    })
+function sendRightClickEvent(clickType) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { defangEvent: clickType });
+  });
 }
